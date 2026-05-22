70c24f54bdfff5ad471ab8a77a7cb441c989a479
Ro <rodrigomanuel.perez@estudiantes.unahur.edu.ar>
2026-05-22 18:35:03 -0300

    fix: elimina importaciones innecesarias en PlanBasico y PlanFamiliar

--- src/main/java/ar/edu/unahur/obj2/w2j/planes/PlanBasicoStrategy.java
+++ src/main/java/ar/edu/unahur/obj2/w2j/planes/PlanBasicoStrategy.java
@@ -1,9 +1,5 @@
 package ar.edu.unahur.obj2.w2j.planes;
 
-import java.util.List;
-
-import ar.edu.unahur.obj2.w2j.contenidos.Contenido;
-
 public class PlanBasicoStrategy extends PlanConLimiteStrategy {
 
     public PlanBasicoStrategy(Integer limiteDeContenidos) {
--- src/main/java/ar/edu/unahur/obj2/w2j/planes/PlanFamiliarStrategy.java
+++ src/main/java/ar/edu/unahur/obj2/w2j/planes/PlanFamiliarStrategy.java
@@ -1,9 +1,5 @@
 package ar.edu.unahur.obj2.w2j.planes;
 
-import java.util.List;
-
-import ar.edu.unahur.obj2.w2j.contenidos.Contenido;
-
 public class PlanFamiliarStrategy extends PlanConLimiteStrategy {
     private Double descuento = 15.0;
 
